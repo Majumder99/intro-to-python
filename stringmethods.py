11293225eba4777737fe662c2751bf903e87e411
Sourav Majumder <souravmajumderk99@gmail.com>
2023-05-18 00:41:15 +0600

    string methods

---
+++ stringmethods.py
@@ -0,0 +1,12 @@
+course = "python for beginners"
+
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.capitalize())
+print(course.find('f'))
+print(course.replace("for", "in"))
+print(course.replace('o', 'k'))
+print(course)
+print("Python" in course)
+print("python" in course)
